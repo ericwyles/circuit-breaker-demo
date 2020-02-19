776062f46eb1ebdd3f00d4003336bb82b3ba53cb
Eric Wyles <eric.wyles@jbhunt.com>
2020-02-19 15:07:20 -0600

    refactoring

--- src/main/java/com/ericwyles/circuitbreakerdemo/HelloService.java
+++ src/main/java/com/ericwyles/circuitbreakerdemo/HelloService.java
@@ -11,39 +11,26 @@ import java.util.concurrent.atomic.AtomicLong;
 @Slf4j
 
 public class HelloService {
-    AtomicLong numCalls= new AtomicLong(0);
-    boolean makeSlow=true;
+    SlownessSimulator slownessSimulator;
+
+    public HelloService(SlownessSimulator slownessSimulator) {
+        this.slownessSimulator = slownessSimulator;
+    }
 
     @CircuitBreaker(name = "endpoint1CircuitBreaker", fallbackMethod = "getHello1OutputFallback")
     public String getHello1Output() {
         log.info("Executing endpoint1 Primary Path.");
-        slowtoggle(true); // toggle slow and sleep for long or short amount of time
+        slownessSimulator.toggleAndSleep(true);
         return "Hello from endpoint 1";
     }
 
     private String getHello1OutputFallback(Exception ex) {
         log.info("Executing endpoint1 Fallback.");
-        slowtoggle(false); // toggle slow boolean but don't sleep
+        slownessSimulator.toggleAndSleep(false);
         return "HELLO SERVICE IS UNAVAILABLE. PLEASE TRY AGAIN LATER.";
     }
 
     public String getHello2Output() {
         return "Hello from endpoint2";
-    }
-
-    private boolean slowtoggle(boolean sleep) {
-        if (numCalls.incrementAndGet() % 50 == 0) { // alternate the flag every 50 calls
-            makeSlow = !makeSlow;
-            log.info("makeSlow is {}", makeSlow);
-        }
-
-        if (sleep) {
-            if (makeSlow)
-                Try.run(() -> Thread.sleep(30000)); // 30 seconds
-            else
-                Try.run(() -> Thread.sleep(15000)); // 15 seconds
-        }
-
-        return makeSlow;
     }
 }
