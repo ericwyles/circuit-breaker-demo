89842a8c4c0bd05e5f6b5af5c1571c83bb5b5072
Eric Wyles <eric.wyles@jbhunt.com>
2020-02-20 09:41:40 -0600

    refactoring

--- scripts/endpoint2_client.sh
+++ scripts/endpoint2_client.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+n=1
+
 while [ true ]
 do
     SECONDS=0
-    echo "Calling endpoint2..."
+    echo "Calling endpoint2. Attempt #$n..."
     RESULT=$(curl -s http://localhost:8080/endpoint2)
 	echo "Endpoint 2 returned '$RESULT' in $SECONDS seconds..."
 	sleep 1
 	echo
+	n=$(( n+1 ))
 done
